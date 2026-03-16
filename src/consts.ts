a409ffddf777de66cbc4bed13a99231b6568f04d
Carlos Martens <carlosmartens1@gmail.com>
2026-03-16 14:39:21 -0500

    background edits

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Martens Driving School";
+export const SITE_DESCRIPTION = "Welcome to Martens Driving School, where we are dedicated to providing top-notch driving education and training. Our experienced instructors are committed to helping you become a confident and safe driver. Whether you're a beginner or looking to improve your skills, we offer comprehensive courses tailored to your needs. Join us today and take the first step towards earning your driver's license!";
