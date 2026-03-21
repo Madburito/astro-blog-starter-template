217199a295bd2703a775041c914330cf0c911a94
Carlos Martens <carlosmartens1@gmail.com>
2026-03-21 15:01:28 -0500

    seo optimization

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Martens Driving School";
-export const SITE_DESCRIPTION = "Welcome to Martens Driving School, where we are dedicated to providing top-notch driving education and training. Our experienced instructors are committed to helping you become a confident and safe driver. Whether you're a beginner or looking to improve your skills, we offer comprehensive courses tailored to your needs. Join us today and take the first step towards earning your driver's license!";
+export const SITE_DESCRIPTION = "Martens Driving School offers one-on-one driving lessons, road test preparation, and road test car rental in Steinbach and surrounding communities.";
